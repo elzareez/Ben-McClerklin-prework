ff67b8004b36fc697c6a777fa118a865654a3308
Ben McClerklin <mcclerklinben@gmail.com>
2020-03-12 12:15:51 -0500

    activity-2 completed

---
+++ Javascript_Basics_Assessment/activity-2.js
@@ -0,0 +1,14 @@
+const ans1 = 9*9;
+const ans2 = 56/12;
+const ans3 = 281%9;
+const ans4 = 18/0;
+const str1 = '56';
+const str2 = '92';
+const ans5 = str1 + str2;
+const ans6 = 42 > 52;
+alert(`'The product of 9 and 9 is ${(ans1)}.
+The Quotient of 56 and 12 is ${ans2}.
+The remainder of 281 divided by 9 is ${ans3}.
+18 divided by 0 is ${ans4}.
+The sum of string 56 and string 92 is ${ans5}.
+$42>52 evaluates to be ${ans6}.`);
