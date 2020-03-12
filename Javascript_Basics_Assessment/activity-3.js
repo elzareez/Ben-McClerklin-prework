7b4ac7fab2f87a835353cc1e9eb07ab37f1ffe31
Ben McClerklin <mcclerklinben@gmail.com>
2020-03-12 12:20:00 -0500

    activity-3 completed

---
+++ Javascript_Basics_Assessment/activity-3.js
@@ -0,0 +1,7 @@
+const userI = prompt('Please input your first name');
+if(userI.length > 4){
+    alert(`Your name is greater than 4 characters.`);
+}
+else{
+    alert(`Your name is less than 4 characters.`);
+}
