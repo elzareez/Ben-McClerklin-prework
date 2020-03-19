a55e710f2db9cb18754c1f7eccede9108b498d06
Ben McClerklin <mcclerklinben@gmail.com>
2020-03-19 13:04:38 -0500

    Final project done

--- Module-2_Assessment/assets/javascript/game.js
+++ Module-2_Assessment/assets/javascript/game.js
@@ -6,6 +6,19 @@ let correctLetter = [];
 let guess = [];
 let wins =0;
 let guessRem = 10;
+let mySound;
+
+//Sound Variables
+var sora= document.getElementById("Sora");
+var donald = document.getElementById("Donald");
+var goofy= document.getElementById("Goofy");
+var kairi= document.getElementById("Kairi");
+var roxas= document.getElementById("Roxas");
+var axel= document.getElementById("Axel");
+var mickey= document.getElementById("Mickey");
+var ventus= document.getElementById("Ventus");
+var aqua= document.getElementById("Aqua");
+var riku= document.getElementById("Riku");
 
 //Game Start Function
 let gameStart = function(){
@@ -16,13 +29,6 @@ let gameStart = function(){
     }
     document.getElementById("cWord").innerText =  correctLetter.join(" ");
 }
-/* Generate random word
-    split word in seperate array
-    store word length for later reference
-    genereate blanks for each letter
-    change the HTML to show blanks
-
-    console log stuff just in case its needed */
 
 //Reset the game
 let reset = function(){
@@ -53,8 +59,6 @@ let checkFunc = function(guessLetter){
     }
 }
 
-
-
 //Won/Lose function
 let winLose = function(){
     if(correctLetter.join() === letterSplit.join()){
@@ -64,21 +68,156 @@ let winLose = function(){
     }
     else if(guessRem === 0){
         reset();
-        document.getElementById("lose").innerText = "You lost refresh to try again";
+        document.getElementById("lose").innerText = "You lost try again";
 
     }
     document.getElementById("cWord").innerText = correctLetter.join(" ")
     document.getElementById("numGuess").innerText = " " + guessRem;
 }
-//Run winLose to see if it works DELETE AFTER
-
 
 //Run game
 gameStart()
 
-document.onkeyup = function(event){
+//Picture function
+function picture() {
+    if(currentWord == words[0]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Sora.png";
+        sora.play();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[1]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Donald.png";
+        sora.pause();
+        donald.play();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[2]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Goofy.png";
+        sora.pause();
+        donald.pause();
+        goofy.play();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[3]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Kairi.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.play();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[4]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Roxas.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.play();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[5]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Axel.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.play();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[6]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Mickey.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.play();
+        ventus.pause();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[7]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Ventus.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.play();
+        aqua.pause();
+        riku.pause();
+    }
+    else if(currentWord == words[8]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Aqua.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.play();
+        riku.pause();
+    }
+    else if(currentWord == words[9]){
+        document.getElementById("hint").src = "../Module-2_Assessment/assets/images/Riku.png";
+        sora.pause();
+        donald.pause();
+        goofy.pause();
+        kairi.pause();
+        roxas.pause();
+        axel.pause();
+        mickey.pause();
+        ventus.pause();
+        aqua.pause();
+        riku.play();
+    }
+  }
+  
+document.onkeypress = function(event){
     let guessLetter = event.key.toLowerCase();
     checkFunc(guessLetter);
     winLose();
+    picture();
     document.getElementById("lettersGuessed").innerText = guess.join(" ");
+    
 }
+
